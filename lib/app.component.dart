823a3df0d9f8bae3761cc5874dd9128dd425e566
Maximilian Riegler <mriegler@Lyoness.local>
2016-11-03 14:31:02 +0100

    initial stuff for dart/angular2

---
+++ lib/app.component.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'my-app',
+    template: 'more comes here'
+)
+class AppComponent {}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular2/platform/browser.dart';
+import 'package:matirc/app.component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
